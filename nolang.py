2acc2a88259263c52f274ecdb0cc824401e6f8fa
Dan Stowell <dans@spotify.com>
2016-05-30 14:07:42 -0400

    verbose, unclean, but can multiply, divide, subtract

--- nolang.py
+++ nolang.py
@@ -1,30 +1,174 @@
 import collections
-import pprint
+import decimal
 
-def noeval(s, env):
-    words = s.split()
-    nwords = len(words)
-    for i in xrange(nwords):
-        word = words[i]
-        if 'is' == word and (i > 0) and (i + 1 < nwords):
-            last = words[i - 1]
-            rest = tuple(words[i + 1:])
-            env[last].append(rest)
-    return env
+
+class Empty(object):
+    def word(self, word, sym2val):
+        try:
+            return Number(decimal.Decimal(word))
+        except decimal.InvalidOperation:
+            if 'is' == word:
+                #TODO: How do we handle this omission?
+                return Error('What is is?')
+            elif word in sym2val:
+                return sym2val[word][-1]
+            else:
+                return Symbol(word)
+
+    def response(self):
+        return 'Empty expression'
+
+
+class Program(object):
+    def __init__(self):
+        self.messages = []
+        self.responses = []
+        self.sym2val = collections.defaultdict(list)
+        self.thats = []
+
+    def message(self, message):
+        words = message.split()
+        last = Empty()
+        for word in words:
+            last = last.word(word, self.sym2val)
+        response = last.response()
+
+        self.messages.append(message)
+        self.responses.append(response)
+        self.thats.append(last)
+
+        return response
+
+
+class Symbol(object):
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+    def word(self, word, sym2val):
+        if 'is' == word:
+            return Is(self.symbol)
+        else:
+            #TODO: do we handle numbers differently?
+            return Error("Sorry, I don't (yet) know what to do with '%s' and '%s' together" % (self.symbol, word))
+
+    def response(self):
+        return 'Symbol ' + self.symbol
+
+
+class Is(object):
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+    def word(self, word, sym2val):
+        try:
+            number = Number(decimal.Decimal(word))
+            sym2val[self.symbol].append(number)
+            return number #TODO: this prevents complex expressions
+        except decimal.InvalidOperation:
+            if word in sym2val:
+                sym2val[self.symbol].append(sym2val[word][-1])
+            else:
+                #TODO: can symbols point to symbols?
+                return Error("Sorry, I don't want to point '%s' to '%s' when I don't know what '%s' means" %  (self.symbol, word, word))
+
+    def response(self):
+        return self.symbol + ' is . . .'
+
+class Error(object):
+    def __init__(self, message):
+        self.message = message
+
+    def word(self, word, sym2val):
+        return self
+
+    def response(self):
+        return self.message
+
+
+class Number(object):
+    def __init__(self, number):
+        self.number = number
+
+    def word(self, word, sym2val):
+        if 'times' == word:
+            return Times(self.number)
+        elif 'over' == word:
+            return Over(self.number)
+        elif 'minus' == word:
+            return Minus(self.number)
+        else:
+            return Error("I don't know what to do with the number %s and the word %s" % (str(self.number), word))
+
+    def response(self):
+        return str(self.number)
+
+
+class Times(object):
+    def __init__(self, left):
+        self.left = left
+
+    def word(self, word, sym2val):
+        try:
+            number = decimal.Decimal(word)
+            return Number(self.left * number)
+        except decimal.InvalidOperation:
+            if word in sym2val:
+                return Number(self.left * sym2val[word][-1].number)
+            else:
+                return Error("I don't know how to multiply %s and %s" % (self.left, word))
+
+    
+    def response(self):
+        return str(self.left) + ' times . . .'
+
+
+class Over(object):
+    def __init__(self, left):
+        self.left = left
+
+    def word(self, word, sym2val):
+        try:
+            number = decimal.Decimal(word)
+            return Number(self.left / number)
+        except decimal.InvalidOperation:
+            if word in sym2val:
+                return Number(self.left / sym2val[word][-1].number)
+            else:
+                return Error("I don't know how to divide %s by %s" % (self.left, word))
+
+    def response(self):
+        return str(self.left) + ' over . . .'
+
+
+class Minus(object):
+    def __init__(self, left):
+        self.left = left
+
+    def word(self, word, sym2val):
+        try:
+            number = decimal.Decimal(word)
+            return Number(self.left - number)
+        except decimal.InvalidOperation:
+            if word in sym2val:
+                return Number(self.left - sym2val[word][-1].number)
+            else:
+                return Error("I don't know how to subtract %s from %s" % (word, self.left))
+
+    def response(self):
+        return str(self.left) + ' minus . . .'
 
 
 def main():
-    env = collections.defaultdict(list)
+    program = Program()
     while True:
         try:
-            s = raw_input('> ')
+            message = raw_input('> ')
         except EOFError:
             break
         except KeyboardInterrupt:
             print
             continue
-        env = noeval(s, env)
-        pprint.pprint(env)
+        print program.message(message)
 
 
 if __name__ == '__main__':
